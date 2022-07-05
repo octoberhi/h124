c8da576e0f44bf400daf8ccbbb53335e8afc2d94
octoberhi <89272971+octoberhi@users.noreply.github.com>
2022-07-05 14:56:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
+function setup()
+{
+canvas = createCanvas(400,400);
+canvas.position(800,300);
+Video = createCapture(VIDEO);
+poseNet = ml5.poseNet(Video,modelLoaded);
+poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded()
+{
+console.log("Model is loaded");
+}
+
+function gotPoses(results)
+{
+if(results.length > 0){
+console.log(results)
+noseX =results[0].pose.nose.x;
+noseY =results[0].pose.nose.y;
+
+leftwristX = results[0].pose.leftwrist.x;
+rightwristx = results[0].pose.rightwrist.x;
+
+difference = floor(leftwristX-rightwristX);
+document.getElementById("name_size").innerHTML = "font size is"+difference+"px";
+}
+}
+function draw()
+{
+background("gray");
+fill("black");
+textSize(difference);
+text("Derek is fine at coding",noseX,noseY);
+}
